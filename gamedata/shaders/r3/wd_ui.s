c4c90ad3a94464b947b3f6cc220e148a564221b7
sulikz <lsuliborski@gmail.com>
2026-09-17 00:19:58 +0200

    0.8.11

--- gamedata/shaders/r3/wd_ui.s
+++ gamedata/shaders/r3/wd_ui.s
@@ -1,7 +1,7 @@
 function normal		(shader, t_base, t_second, t_detail)
 	shader:begin	("wd_ui","wd_ui")
 			: fog		(false)
-			: zb 		(true,false)
+			: zb 		(true,true)
 			: blend		(true,blend.srcalpha,blend.invsrcalpha)
 			: dx10color_write_enable( true, true, true, false)
 	shader:dx10texture	("s_base", t_base)
